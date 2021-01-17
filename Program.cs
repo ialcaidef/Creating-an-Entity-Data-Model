5b7bab688f858d6e2040aa3293e4b45615784abf
Alcaide F. Inocente <ialcaidef@hotmail.com>
2021-01-17 08:48:35 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MyFirstEF.Database;
+
+namespace MyFirstEF
+{
+    class Program
+    {
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }*/
+        static void Main(string[] args)
+        {
+            using (var context = new MyDbContext())
+            {
+                DbInitializer.Initialize(context);
+            }
+            Console.WriteLine("Database created");
+        }
+    }
+}
